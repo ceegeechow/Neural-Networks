ad5054c9d96e8d826159b7041592f199c33acaf4
ceegeechow <camille@chowww.com>
2018-10-02 15:42:26 -0400

    ready to submit

--- cifar100.py
+++ cifar100.py
@@ -2,7 +2,6 @@
 #ECE 471 Assignment 4
 #Classifying CIFAR100 data
 #Citation: Learning Multiple Layers of Features from Tiny Images, Alex Krizhevsky, 2009.
-
 import numpy as np
 import tensorflow as tf
 
@@ -11,8 +10,6 @@ num_classes = 100
 image_h = 32
 image_w = 32
 channels = 3
-#input_shape = (image_h, image_w, channels)
-#val_set_size = 10000
 
 #get data
 (x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar100.load_data()
@@ -34,7 +31,7 @@ batch_size = 100
 epochs = 10
 dropout = .5
 dense_units = 1000
-lam = .003
+lam = .001
 
 def add_conv_layer(model, num_filters):
 	model.add(tf.keras.layers.Conv2D(num_filters, kernel_size=3, 
@@ -48,24 +45,15 @@ def add_bn_layer(model):
 
 #build cnn
 model = tf.keras.Sequential()
-#add_conv_layer(model, 16)
-#add_pooling_layer(model)
-#add_conv_layer(model, 32)
-#add_pooling_layer(model)
 add_conv_layer(model, 64)
 add_bn_layer(model)
 add_pooling_layer(model)
 add_conv_layer(model, 32)
 add_bn_layer(model)
 add_pooling_layer(model)
-# add_conv_layer(model, 32)
-# add_bn_layer(model)
-# add_pooling_layer(model)
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(dense_units, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(lam)))
 model.add(tf.keras.layers.Dropout(dropout))
-# model.add(tf.keras.layers.Dense(dense_units, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(lam)))
-# model.add(tf.keras.layers.Dropout(dropout))
 model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
 
 #train model
